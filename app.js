936edba01ceeebb8e10e439e99a0112a3acc6e6a
ameer suhail <kameer@gmail.com>
2022-11-23 22:21:37 +0530

    added thidayname to ejs file in order to render dayname too

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ app.get("/", (req, res) => {
   var today = new Date();
   var currentDay = today.getDay();
   var day = " ";
+  var dayName = today.toLocaleString("en-US", { weekday: "long" });
   if (currentDay === 6 || currentDay === 0) {
     day = "weekend";
   } else {
     day = "weekday";
   }
   // * in the below method current day is the marker in ejs file in the views folder
-  res.render("list", { thisDay: day });
+  res.render("list", { thisDay: day, thisDayName: dayName });
 });
 
 app.listen(port, () => {
