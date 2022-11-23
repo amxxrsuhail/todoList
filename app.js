0fa4fe8f8c60822122aabe78e8c8b6e12006540f
ameer suhail <kameer@gmail.com>
2022-11-23 21:18:14 +0530

    used ejs to render whether its a weekday or weekend

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = " ";
+  if (currentDay === 6 || currentDay === 0) {
+    day = "weekend";
+  } else {
+    day = "weekday";
+  }
+  // * in the below method current day is the marker in ejs file in the views folder
+  res.render("list", { thisDay: day });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server started listening on port ${port}`);
 });
