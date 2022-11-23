67218e1f0d223c660e8e5e47d4ea6446d3f9104d
ameer suhail <kameer@gmail.com>
2022-11-24 01:18:57 +0530

    added for loop to add a new list on every append in listAdd

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+var Todos = [];
 
 app.set("view engine", "ejs");
+
+// * below line is must have when u wanna get values from the user
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   var today = new Date();
   var currentDay = today.getDay();
   var day = " ";
-  var dayName = today.toLocaleString("en-US", { weekday: "long" });
-  if (currentDay === 6 || currentDay === 0) {
-    day = "weekend";
-  } else {
-    day = "weekday";
-  }
+  var dayName = today.toLocaleString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  });
   // * in the below method current day is the marker in ejs file in the views folder
-  res.render("list", { thisDay: day, thisDayName: dayName });
+  res.render("list", { thisDayName: dayName, newItem: Todos });
+});
+
+app.post("/", (req, res) => {
+  newToDo = req.body.listAdd;
+  Todos.push(newToDo);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
