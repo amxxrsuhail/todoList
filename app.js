dff6086de79698d6fec19050b9ad2768059d446e
ameer suhail <kameer@gmail.com>
2022-12-02 18:06:07 +0530

    set up mongo atlas added changes to mongo connect

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // !-------------database boilerplate---------------
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/toDoListDB");
+mongoose.connect("mongodb+srv://ameersuhail:AMINAdavood41099@cluster0.czyujet.mongodb.net/toDoListDB");
 
 const itemSchema = { item: { type: String, required: true } };
 
